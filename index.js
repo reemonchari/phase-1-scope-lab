6321b19f71799f7a154887c8a6325a55d8387a8b
+ <+>
2024-10-01 16:43:01 +0300

    Scope lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Write your solution in this file!
+var customerName = 'bob'
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase()
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob'
+}
+function overwriteBestCustomer(){
+    return bestCustomer = 'maybe bob'
+}
+
+const leastFavoriteCustomer = 'mat'
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'becca'
+}
